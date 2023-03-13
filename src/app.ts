cec669ce819504093aeace8ef7a1b22a3911363a
chiharakenta <chiharakenta0519@gmail.com>
2023-03-13 13:55:56 +0900

    feat: create User schema & model

--- src/app.ts
+++ src/app.ts
@@ -8,11 +8,9 @@ app.get('/', (req, res) => {
   res.send(process.env.PORT);
 });
 
-app.get('/ip', async (req, res) => {
-  const resp = await fetch('https://api.ipify.org?format=json');
-  const json = await resp.json();
-  res.json(json);
-});
+// ユーザー新規登録API
+
+// ユーザーログインAPI
 
 if ((process.env.NODE_ENV = 'production')) {
   app.listen(process.env.PORT ? parseInt(process.env.PORT, 10) : 3000);
---
+++ src/v1/models/user.ts
@@ -0,0 +1,20 @@
+import { model, Schema } from 'mongoose';
+
+type UserType = {
+  username: string;
+  password: string;
+};
+
+const userSchema = new Schema<UserType>({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+export const UserModel = model<UserType>('User', userSchema);
