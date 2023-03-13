072118f6219680996033c26e3e4286200b5dea35
chiharakenta <chiharakenta0519@gmail.com>
2023-03-13 17:07:00 +0900

    feat: add validation check when regist user

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import { UserModel, UserType } from './v1/models/user';
 import AES from 'crypto-js/aes';
 import { MONGODB_URL, NODE_ENV, PORT, SECRET_KEY, TOKEN_SECRET_KEY } from './v1/constants/env';
 import { sign } from 'jsonwebtoken';
+import { body, validationResult } from 'express-validator';
 
 const app = express();
 app.use(bodyParser.json());
@@ -18,19 +19,39 @@ app.use(bodyParser.urlencoded({ extended: true }));
 })().catch((error) => console.error(error));
 
 // ユーザー新規登録API
-app.post('/register', async (req: Request<any, any, UserType>, res) => {
-  try {
-    const { username, password } = req.body;
-    const encryptedPassword = AES.encrypt(password, SECRET_KEY);
-    const user = await UserModel.create({ username, password: encryptedPassword });
-    const token = sign({ id: user._id }, TOKEN_SECRET_KEY, {
-      expiresIn: '24h'
+app.post(
+  '/register',
+  body('username').isLength({ min: 8 }).withMessage('ユーザー名は8文字以上で入力してください。'),
+  body('password').isLength({ min: 8 }).withMessage('パスワードは8文字以上で入力してください。'),
+  body('confirmPassword')
+    .isLength({ min: 8 })
+    .withMessage('確認用パスワードは8文字以上で入力してください。'),
+  body('username').custom((value) => {
+    return UserModel.findOne({ username: value }).then((user) => {
+      if (user) return Promise.reject('このユーザー名は既に使われています。');
     });
-    return res.status(200).json({ user, token });
-  } catch (error) {
-    return res.status(500).json(error);
+  }),
+  (req, res, next) => {
+    const validationErrors = validationResult(req);
+    if (!validationErrors.isEmpty()) {
+      return res.status(400).json({ errors: validationErrors.array() });
+    }
+    next();
+  },
+  async (req: Request<any, any, UserType>, res) => {
+    try {
+      const { username, password } = req.body;
+      const encryptedPassword = AES.encrypt(password, SECRET_KEY);
+      const user = await UserModel.create({ username, password: encryptedPassword });
+      const token = sign({ id: user._id }, TOKEN_SECRET_KEY, {
+        expiresIn: '24h'
+      });
+      return res.status(200).json({ user, token });
+    } catch (error) {
+      return res.status(500).json(error);
+    }
   }
-});
+);
 
 // ユーザーログインAPI
 
