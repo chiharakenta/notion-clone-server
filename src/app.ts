a7e08ee3077c672f4fbcbfe9e2fed0575557c3da
chiharakenta <chiharakenta0519@gmail.com>
2023-03-13 16:35:42 +0900

    feat: jsonwebtoken

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import bodyParser from 'body-parser';
 import * as dotenv from 'dotenv';
 dotenv.config();
-import express from 'express';
+import express, { Request } from 'express';
 import { connect } from 'mongoose';
 import { UserModel, UserType } from './v1/models/user';
 import AES from 'crypto-js/aes';
-import { MONGODB_URL, NODE_ENV, PORT, SECRET_KEY } from './v1/constants/env';
+import { MONGODB_URL, NODE_ENV, PORT, SECRET_KEY, TOKEN_SECRET_KEY } from './v1/constants/env';
+import { sign } from 'jsonwebtoken';
 
 const app = express();
 app.use(bodyParser.json());
@@ -17,15 +18,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 })().catch((error) => console.error(error));
 
 // ユーザー新規登録API
-app.post('/register', async (req, res) => {
+app.post('/register', async (req: Request<any, any, UserType>, res) => {
   try {
-    const { username, password } = req.body as UserType;
+    const { username, password } = req.body;
     const encryptedPassword = AES.encrypt(password, SECRET_KEY);
     const user = await UserModel.create({ username, password: encryptedPassword });
-    res.json(user);
+    const token = sign({ id: user._id }, TOKEN_SECRET_KEY, {
+      expiresIn: '24h'
+    });
+    return res.status(200).json({ user, token });
   } catch (error) {
-    console.error(error);
-    res.json(error);
+    return res.status(500).json(error);
   }
 });
 
--- src/v1/constants/env.ts
+++ src/v1/constants/env.ts
@@ -2,3 +2,4 @@ export const NODE_ENV = process.env.NODE_ENV!;
 export const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 export const MONGODB_URL = process.env.MONGODB_URL!;
 export const SECRET_KEY = process.env.SECRET_KEY!;
+export const TOKEN_SECRET_KEY = process.env.TOKEN_SECRET_KEY!;
