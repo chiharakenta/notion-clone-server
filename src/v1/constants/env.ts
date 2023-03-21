de5bc6549c6df708577e78a1dd8ab9045e03b35a
chiharakenta <chiharakenta0519@gmail.com>
2023-03-21 16:35:58 +0900

    fix: cors origin switching

--- src/v1/constants/env.ts
+++ src/v1/constants/env.ts
@@ -2,3 +2,4 @@ export const NODE_ENV = process.env.NODE_ENV!;
 export const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 export const SECRET_KEY = process.env.SECRET_KEY!;
 export const TOKEN_SECRET_KEY = process.env.TOKEN_SECRET_KEY!;
+export const CORS_ORIGIN = process.env.CORS_ORIGIN!;
